ea96b17a73a17707a07ec3e9caac815abe31391f
nafiz rakib <rakibnafiz6@gmail.com>
2025-01-17 18:37:44 +0600

    added get method with register camp data

--- index.js
+++ index.js
@@ -162,6 +162,13 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/register/:email', async(req, res)=>{
+            const email = req.params.email;
+            const query = {participantEmail: email}
+            const result = await joinCollection.find(query).toArray();
+            res.send(result);
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });

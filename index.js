f49950b2f72a3d154c8b67f20c5e1dc09e9a745a
nafiz rakib <rakibnafiz6@gmail.com>
2025-01-18 21:51:55 +0600

    create delete method with cancel register

--- index.js
+++ index.js
@@ -191,6 +191,14 @@ async function run() {
             res.send(result);
         })
 
+        // registation cancel organizer
+        app.delete('/delete-register/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await joinCollection.deleteOne(query)
+            res.send(result);
+        })
+
         app.delete('/register/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
